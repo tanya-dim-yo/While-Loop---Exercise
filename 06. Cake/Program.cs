78975d8356d2f6a71aeb8d8a8b2baa18bd1ed1c1
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-21 04:58:32 +0300

    Add files via upload

---
+++ 06. Cake/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int cakesPieces = width * length;
+
+            while (cakesPieces > 0)
+            {
+                string currentTake = Console.ReadLine();
+
+                if (currentTake == "STOP")
+                {
+                    break;
+                }
+
+                cakesPieces -= int.Parse(currentTake);
+            }
+
+            if (cakesPieces <= 0)
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(0 - cakesPieces)} pieces more.");
+            }
+            else
+            {
+                Console.WriteLine($"{Math.Abs(cakesPieces - 0)} pieces are left.");
+
+            }
+        }
+    }
+}
