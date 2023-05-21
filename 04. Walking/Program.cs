297aae95f10f7115c22e46a8eedacfae7495b186
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-21 04:56:01 +0300

    Add files via upload

---
+++ 04. Walking/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stepsNeeded = 10000;
+            int stepsToGoal = 0;
+
+            while (stepsToGoal < stepsNeeded)
+            {
+                string currentSteps = Console.ReadLine();
+
+                if (currentSteps == "Going home")
+                {
+                    int finalSteps = int.Parse(Console.ReadLine());
+                    stepsToGoal += finalSteps;
+                    break;
+                }
+
+                stepsToGoal += int.Parse(currentSteps);
+            }
+
+            if (stepsToGoal >= stepsNeeded)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{stepsToGoal - stepsNeeded} steps over the goal!");
+            }
+            else
+            {
+                Console.WriteLine($"{stepsNeeded - stepsToGoal} more steps to reach goal.");
+            }
+        }
+    }
+}
