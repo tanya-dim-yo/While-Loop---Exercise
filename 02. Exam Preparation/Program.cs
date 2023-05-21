f0fe01e366a6091c503e960b9390b3b62a256cb2
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-21 04:54:30 +0300

    Add files via upload

---
+++ 02. Exam Preparation/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ExamPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int poorGradesCount = int.Parse(Console.ReadLine());
+            int poorGradesCounter = 0;
+            int solvedAssignmentsCounter = 0;
+            double gradesSum = 0;
+            bool isFailed = true;
+            string lastProblem = "";
+
+            while (poorGradesCounter < poorGradesCount)
+            {
+                string nextAssignment = Console.ReadLine();
+
+                if (nextAssignment == "Enough")
+                {
+                    isFailed = false;
+                    break;
+                }
+
+                int nextAssignmentGrade = int.Parse(Console.ReadLine());
+
+                if (nextAssignmentGrade <= 4)
+                {
+                    poorGradesCounter++;
+                }
+
+                solvedAssignmentsCounter++;
+                gradesSum += nextAssignmentGrade;
+                lastProblem = nextAssignment;
+            }
+
+            if (isFailed)
+            {
+                Console.WriteLine($"You need a break, {poorGradesCounter} poor grades.");
+            }
+            else
+            {
+                Console.WriteLine($"Average score: {gradesSum / solvedAssignmentsCounter:F2}");
+                Console.WriteLine($"Number of problems: {solvedAssignmentsCounter}");
+                Console.WriteLine($"Last problem: {lastProblem}");
+            }
+        }
+    }
+}
