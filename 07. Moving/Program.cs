e4e6e49ee71273cd1176f2aaa74af18df206b096
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-23 22:01:46 +0300

    Add files via upload

---
+++ 07. Moving/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int area = width * length * height;
+
+            while (area > 0)
+            {
+                string currentBox = Console.ReadLine();
+
+                if (currentBox == "Done")
+                {
+                    break;
+                }
+
+                area -= int.Parse(currentBox);
+            }
+
+            if (area <= 0)
+            {
+                Console.WriteLine($"No more free space! You need {Math.Abs(0 - area)} Cubic meters more.");
+            }
+            else
+            {
+                Console.WriteLine($"{area - 0} Cubic meters left.");
+            }
+        }
+    }
+}
